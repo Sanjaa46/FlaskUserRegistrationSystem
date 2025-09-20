44101c3bf233b294d33ef67f7f7d333b7d869cdf
Sanjaa46 <sanjaas880@gmail.com>
2025-09-20 15:13:32 +0800

    register post request test done

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-@app.route('/api/hello')
-def hello_api():
-    return jsonify({"message": "Hello from API!"})
+# MySQL connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:toor@localhost/user_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if __name__ == '__main__':
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+# Registeration endpoint
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+    if not username or not email or not password:
+        return jsonify({"error", "All fields are required"}), 400
+    
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error", "Email already registered"}), 400
+    
+    hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    new_user = User(username=username, email=email, password=hashed_pw)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully!"}), 201
+
+if __name__ == "__main__":
     app.run(debug=True)
